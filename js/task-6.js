47a78ac8292a224cc50caca09881acc0d132e34b
Yunus Özkaya <92160164+YunusOzkaya@users.noreply.github.com>
2025-03-12 01:10:44 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -4,43 +4,41 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-function createBoxes(amount){
-  const boxesContainer = document.getElementById("boxes")
-  const boxArray =[]
-    for(let i=0; i<amount; i++){
-      const size = 30 + i * 10; // Her bir öğenin boyutu
-      const box = document.createElement('div');
-      box.style.width = `${size}px`;
-      box.style.height = `${size}px`;
-      box.style.backgroundColor = getRandomHexColor();
-      box.style.marginBottom = '10px'; // Öğeler arasında boşluk
-      console.log(box)
-      boxArray.push(box);
-    }
-    boxesContainer.append(...boxArray)
-  
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  const fragment = document.createDocumentFragment(); 
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10; 
+    const box = document.createElement("div");
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.marginBottom = "10px"; 
+    fragment.appendChild(box);
+  }
+
+  boxesContainer.appendChild(fragment); 
 }
 
 function destroyBoxes() {
-  const boxesContainer = document.getElementById('boxes');
-  boxesContainer.innerHTML = ''; // tüm öğeleri temizle
+  document.getElementById("boxes").innerHTML = ""; 
 }
 
-const inputEL= document.querySelector("input[type=number]")
-const createEL = document.querySelector("button[data-create]")
-const destroyEL = document.querySelector("button[data-destroy]")
+const inputEL = document.querySelector("input[type=number]");
+const createEL = document.querySelector("button[data-create]");
+const destroyEL = document.querySelector("button[data-destroy]");
 
-createEL.addEventListener("click",()=>{
-  if(inputEL.value>0 && inputEL.value<101){
-    createBoxes(inputEL.value)
+createEL.addEventListener("click", () => {
+  const amount = parseInt(inputEL.value, 10);
+  
+  if (amount > 0 && amount < 101) {
+    createBoxes(amount);
+  } else {
+    alert("Enter a number between 1-100.");
   }
-  else{
-    alert("Enter a number between 0-100.")
-  }
-})
+});
 
-destroyEL.addEventListener("click",()=>{
-  destroyBoxes()
-})
-
-
+destroyEL.addEventListener("click", () => {
+  destroyBoxes();
+});
